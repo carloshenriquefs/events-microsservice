d743a9368104dd8c179448b80fd5ee01dda5a2c7
Carlos Henrique <ichfs9108@gmail.com>
2023-12-04 03:33:08 -0300

    feat: Adicionando Domains

---
+++ event-microservice/src/main/java/com/microservice/event/domain/Subscription.java
@@ -0,0 +1,24 @@
+package com.microservice.event.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "subscription")
+@Table(name = "subscription")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Event event;
+
+    private String participantEmail;
+
+}
